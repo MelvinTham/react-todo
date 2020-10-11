75256f37b4227dd730a6b8c8b5ab05ab37bb80b5
MelvinTham <melvintham1997@gmail.com>
2020-10-11 12:44:53 -0400

    working todo app

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect,useState} from 'react';
 import './App.css';
+import TodoForm from "./Components/TodoForm";
+import TodoList from "./Components/TodoList";
+import Typography from "@material-ui/core/Typography";
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storagetodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storagetodos){
+       setTodos(storagetodos)
+    }
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(todo){
+    setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id){
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id){
+          return{
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+  function removeTodo(id){
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Typography variant="h1" style ={{padding : 16}}>
+          React Todo
+        </Typography>
+        <TodoForm addTodo={addTodo}> </TodoForm>
+        <TodoList 
+          todos={todos} 
+          toggleComplete={toggleComplete}
+          removeTodo={removeTodo}
+        />
     </div>
   );
 }
